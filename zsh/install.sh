6c2610b891fae1eca4f8592eed58b11dfb25f262
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-04-26 16:09:24 -0300

    Add install.sh in zsh config

---
+++ zsh/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install zsh plugins on linux
+
+# Configs
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp zsh/.zshrc ~
+
+# Themes
+cp -fR ./zsh/themes/spaceship-prompt ~/.oh-my-zsh/custom/themes/spaceship-prompt
+ln -s -f ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+
+# Plugins
+cp -fR ./zsh/plugins/fast-syntax-highlighting ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+cp -fR ./zsh/plugins/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+cp -fR ./zsh/plugins/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+
