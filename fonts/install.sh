ee2f23c6aba9fce93ddc2257e0aca6859bab6905
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-05-14 17:00:47 -0300

    Fix newline

--- fonts/install.sh
+++ fonts/install.sh
@@ -8,7 +8,6 @@ unzip -qq -o ./FiraCode.zip -d ~/.local/share/fonts/
 rm -rf ./FiraCode.zip
 
 # Hack NerdFont
-
 wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
 unzip -qq -o ./Hack.zip -d ~/.local/share/fonts/
 rm -rf ./Hack.zip
