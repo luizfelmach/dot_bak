e47be62bebad56876511bf88b84be78f96da367c
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-05-15 11:28:20 -0300

    Add download submodules

--- zsh/install.sh
+++ zsh/install.sh
@@ -12,6 +12,9 @@ if ! command -v zsh &> /dev/null; then
     exit
 fi
 
+# Download submodules
+git submodule update --init --recursive
+
 # Configs
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
