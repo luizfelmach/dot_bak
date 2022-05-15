aa528031769328efe9839f3bc5fc0e8b40c5b36f
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-05-15 11:21:48 -0300

    Resolve problem absolute path

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,6 +2,9 @@
 
 # Install zsh plugins on linux
 
+SCRIPT=`realpath $0`
+ABS_PATH=`dirname $SCRIPT`
+
 # Verify if zsh exists
 if ! command -v zsh &> /dev/null; then
     echo "Command zsh could not be found!"
@@ -14,19 +17,18 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 fi
 
-cp -fR zsh/zshrc ~/.zshrc
+cp -fR $ABS_PATH/zshrc $HOME/.zshrc
 
 # Themes
-mkdir -p "$HOME/.zsh"
-cp -fR ./zsh/theme/pure ~/.zsh/pure/
+mkdir -p $HOME/.zsh
+cp -fR $ABS_PATH/theme/pure $HOME/.zsh/pure/
 
 # Plugins
-cp -fR ./zsh/plugins/fast-syntax-highlighting ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
-cp -fR ./zsh/plugins/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-cp -fR ./zsh/plugins/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-cp -fR ./zsh/plugins/ls ~/.oh-my-zsh/custom/plugins/ls
-cp -fR ./zsh/plugins/material-colors ~/.oh-my-zsh/custom/plugins/material-colors
+cp -fR $ABS_PATH/plugins/fast-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+cp -fR $ABS_PATH/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+cp -fR $ABS_PATH/plugins/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+cp -fR $ABS_PATH/plugins/ls $HOME/.oh-my-zsh/custom/plugins/ls
+cp -fR $ABS_PATH/plugins/material-colors $HOME/.oh-my-zsh/custom/plugins/material-colors
 
-clear
 exec zsh
 exit
