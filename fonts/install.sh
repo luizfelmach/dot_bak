9e380bd6879cea2109b5fb0de75b7c3ccfc0f48e
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-05-15 12:12:21 -0300

    Resolve problem absolute path

--- fonts/install.sh
+++ fonts/install.sh
@@ -4,11 +4,11 @@
 
 # FiraCode NerdFont
 wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip -qq -o ./FiraCode.zip -d ~/.local/share/fonts/
+unzip -qq -o ./FiraCode.zip -d $HOME/.local/share/fonts/
 rm -rf ./FiraCode.zip
 
 # Hack NerdFont
 wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
-unzip -qq -o ./Hack.zip -d ~/.local/share/fonts/
+unzip -qq -o ./Hack.zip -d $HOME/.local/share/fonts/
 rm -rf ./Hack.zip
 
