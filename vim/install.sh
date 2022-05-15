7f992c97785fdb3ca947db4c98642af36244fbde
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-05-15 11:57:08 -0300

    Resolve problem absolute path

--- vim/install.sh
+++ vim/install.sh
@@ -2,29 +2,32 @@
 
 # Install vim config on linux
 
+SCRIPT=`realpath $0`
+ABS_PATH=`dirname $SCRIPT`
+
 echo "Installation vim/nvim on linux..."
 echo "[1] VIM"
 echo "[2] NVIM"
 read -p "Your option: " option
 
 if (($option == 1)); then
-    mkdir -p ~/.config/vim/
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    mkdir -p $HOME/.config/vim/
+    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    cp vim/* ~/.config/vim/
-    rm -rf ~/.config/vim/install.sh
-    rm -rf ~/.config/vim/nvimrc/
-    mv ~/.config/vim/vimrc ~/.vimrc
+    cp $ABS_PATH/* $HOME/.config/vim/
+    rm -rf $HOME/.config/vim/install.sh
+    rm -rf $HOME/.config/vim/nvimrc/
+    mv $HOME/.config/vim/vimrc $HOME/.vimrc
     vim +PlugInstall +qall
 
 elif (($option == 2)); then
-    mkdir -p ~/.config/nvim/
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    mkdir -p $HOME/.config/nvim/
+    curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    cp vim/* ~/.config/nvim/
-    rm -rf ~/.config/nvim/install.sh
-    rm -rf ~/.config/nvim/vimrc/
-    mv ~/.config/nvim/nvimrc ~/.config/nvim/init.vim
+    cp $ABS_PATH/* $HOME/.config/nvim/
+    rm -rf $HOME/.config/nvim/install.sh
+    rm -rf $HOME/.config/nvim/vimrc/
+    mv $HOME/.config/nvim/nvimrc $HOME/.config/nvim/init.vim
     nvim +PlugInstall +qall
 fi
 
