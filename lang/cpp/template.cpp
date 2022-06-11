fb55cc4eb47ab6885e18ea13b5f215c9f1b1a13e
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-06-11 11:05:02 -0300

    Add template cpp

---
+++ lang/cpp/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define nL "\n"
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define imax INT_MAX
+#define imin INT_MIN
+#define ING 0x3f3f3f3f
+#define exp 1e9
+#define sz(x) (int((x).size()))
+
+int main() { _
+
+    cout << "Ola mundo" << endl;
+    return 0;
+}
