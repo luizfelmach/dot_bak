4ee3a00c154a728700922c5ecb2008d8e12edc16
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-06-19 12:13:11 -0300

    Add download makefile

--- bin/prog.sh
+++ bin/prog.sh
@@ -2,6 +2,8 @@
 
 FILENAME=${1:-template}
 URL="https://raw.githubusercontent.com/luizfelmach/dotfiles/main/lang/cpp/template.cpp"
+URL_MAKEFILE="https://raw.githubusercontent.com/luizfelmach/dotfiles/main/lang/cpp/makefile"
 
 curl -o "$FILENAME.cpp" $URL
+curl -o makefile $URL_MAKEFILE
 
